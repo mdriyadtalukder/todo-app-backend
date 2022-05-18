e8b6cdb2e32e4a2171d63c5bd678f0d5e999ced5
mdriyadtalukder <riyadtalukder67@gmail.com>
2022-05-18 23:44:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+var cors = require('cors');
+const app = express();
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gup0d.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const usersCollection = client.db("todoapp").collection("products");
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = usersCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+        app.post('/products', async (req, res) => {
+            const newUser = req.body;
+            console.log(newUser);
+            const result = await usersCollection.insertOne(newUser);
+            res.send(result);
+
+        });
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            res.send(result);
+        });
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello to-do app!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
